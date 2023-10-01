93b5bf81f63e8eccc2142ceeeacc9f780e6f841f
ttdo19 <thuytrang19072001@gmail.com>
2023-10-01 01:06:16 -0500

    Added the 'I'm feeling lucky button" and change background-image without reloading page

--- Fall2023-Assignment2-ttdo/wwwroot/js/script.js
+++ Fall2023-Assignment2-ttdo/wwwroot/js/script.js
@@ -1,3 +1,5 @@
+let darkModeOn = 1;
+
 function apiSearch() {
     var params = {
         'q': $('#query').val(),
@@ -24,7 +26,6 @@ function apiSearch() {
 
             $('#searchResults').html(results);
             $('#searchResults').dialog({
-           
                 width: 1000,
                 height: 450,
                 title: 'Search Results for \"' + $('#query').val() + '\"',
@@ -36,6 +37,64 @@ function apiSearch() {
         });
 }
 
+function getCurrentTime() {
+    const date = new Date();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    var time = 'The current time is ' + hour + ':' + min + '.';
+    $('#time').css('visibility', 'visible');
+    $('#time').html(time);
+    $('#time').dialog({
+        title: 'Current Time',
+        width: 300,
+        height: 100,
+        position: { my: "right top", at: "right top", of: ".hero", collision: "none" }
+    })
+}
+
+function changeBackground() {
+    if (darkModeOn) {
+        $('.hero').css('background-image', 'url(\"../Nature.jpg\")');
+        $('#searchEngineName').css('color', 'black');
+        darkModeOn = 0; 
+    } else {
+        $('.hero').css('background-image', 'url(\"../Night_Sky.jpg\")');
+        $('#searchEngineName').css('color', 'white');
+        darkModeOn = 1; 
+    }
+}
+
 $('#searchButton').click(function () {
     apiSearch(); 
 }); 
+
+$('#timeButton').click(function () {
+    getCurrentTime(); 
+});
+
+$('#searchEngineName').click(function () {
+    changeBackground(); 
+})
+
+$('#luckyButton').click(function () {
+    var params1 = {
+        'q': $('#query').val(),
+        'count': 1,
+        'offset': 0,
+        'mkt': 'en-us'
+    };
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params1),
+        type: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': '309290b7c24f43e4af89c2f0be560e6a'
+        }
+    })
+        .done(function (data) {
+            window.open(data.webPages.value[0].url, '_blank'); 
+        })
+        .fail(function () {
+            alert('error');
+        });
+})
+
